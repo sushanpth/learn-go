ed3bc193b0dbae0c2e2adb8489539b772bf7a67b
Sudarshan Pathak <sushan.pth@gmail.com>
2023-05-22 17:14:03 +0545

    create demo go-redis project

---
+++ redis-elastic-cache/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
+)
+
+var ctx = context.Background()
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	rdb := redis.NewClient(&redis.Options{
+		Addr: os.Getenv("REDIS_SERVER"),
+		// Password:     "password",
+		// Addr:         "localhost:6379",
+		PoolSize:     10,
+		MinIdleConns: 10,
+
+		DialTimeout:  5 * time.Second,
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 3 * time.Second,
+		PoolTimeout:  4 * time.Second,
+
+		MaxRetries:      10,
+		MinRetryBackoff: 8 * time.Millisecond,
+		MaxRetryBackoff: 512 * time.Millisecond,
+
+		// TLSConfig: &tls.Config{
+		// 	InsecureSkipVerify: true,
+		// },
+		OnConnect: func(ctx context.Context, cn *redis.Conn) error {
+			fmt.Println("Connected to redis server.", cn)
+			return nil
+		},
+	})
+
+	err = rdb.Set(ctx, "key", "Value String", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
+
+	val2, err := rdb.Get(ctx, "key2").Result()
+	if err == redis.Nil {
+		fmt.Println("key2 does not exist")
+	} else if err != nil {
+		panic(err)
+	} else {
+		fmt.Println("key2", val2)
+	}
+	// Output: key value
+	// key2 does not exist
+
+}
